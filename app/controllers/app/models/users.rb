8d7b62632c7bba565dcfc1b65a3a9e1fdc61754e
EC2 Default User <ec2-user@ip-172-31-2-39.ap-northeast-1.compute.internal>
2022-12-22 08:18:17 +0000

    バリデーションが効かない

--- app/controllers/app/models/users.rb
+++ app/controllers/app/models/users.rb
@@ -1,4 +1,9 @@
- class Schedule < ApplicationRecord
-   validates :title, presence: true
-   require Rails.root.join
- end
+class Schedule < ApplicationRecord
+  validate :start_finish_check
+  
+  def start_finish_check
+    if self.start >= self.finish
+     errors.add(:finish, "は開始日より前の日付は登録できません") 
+    end
+  end
+end
